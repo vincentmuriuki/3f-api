cb1714591e107abacd82043c85a44d4644d99eb7
tesh254 <ewachira254@gmail.com>
2018-09-17 22:10:49 +0300

    Api for updating an order status and test are done and passing [#160346732]

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -16,7 +16,35 @@ orders = db.get_orders()
 
 class OrdersManipulation(Resource):
     def put(self, identifier):
-        pass
+
+        order = [order for order in orders if order['id'] == identifier]
+        if len(order) == 0:
+            return (
+                {
+                    "message":"Order of the identifier not found"
+                }
+            ), 404
+        elif not request.json:
+            return (
+                {
+                    "message":"Missing a field"
+                }
+            ), 400
+        elif 'status' in request.json and type(request.json['status']) != bool:
+            return (
+                {
+                    "message":"Missing a field"
+                }
+            ), 400
+        else:
+            order[0]['status'] = request.json.get('status', order[0]['status'])
+
+            return (
+                {
+                    'message':'Order delivered',
+                    'order': order[0]
+                }
+            ), 201
 
 api.add_resource(OrdersManipulation, '/api/v1/orders/<int:identifier>')
 
