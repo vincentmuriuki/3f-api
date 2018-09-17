98c993549351112b730b2c01b7f977d91703c509
tesh254 <ewachira254@gmail.com>
2018-09-17 22:17:49 +0300

    The delete api was not declared [#160550030]

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -16,7 +16,20 @@ orders = db.get_orders()
 
 class OrdersManipulation(Resource):
     def delete(self, identifier):
-        pass
+        order = [order for order in orders if order['id'] == identifier]
+        if len(order) == 0:
+            return (
+                {
+                    "message":"Order of the id not found"
+                }
+            ), 404
+        else:
+            order.remove(order[0])
+            return (
+                {
+                    "message":"Success, order deleted"
+                }
+            ), 204
 
 api.add_resource(OrdersManipulation, '/api/v1/orders/<int:identifier>')
 
