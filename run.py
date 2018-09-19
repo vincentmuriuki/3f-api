e53c133d14faaf3da8657a1aef31d1af0799ea3f
tesh254 <ewachira254@gmail.com>
2018-09-19 17:20:32 +0300

    Added the instance folder for a successful build on travis [#160586113]

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from app import create_app
 
 
-app = create_app(os.getenv("CONFIG_TYPE") or "default")
+app = create_app(os.getenv("CONFIG_TYPE") or "production")
 
 if __name__ == "__main__":
     app.run(debug=True)
