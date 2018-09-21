82946fa175852c2390a6b2718b559e57f666c62e
tesh254 <ewachira254@gmail.com>
2018-09-21 17:30:50 +0300

    Requirements.txt update [#160683365]

--- app/database.py
+++ app/database.py
@@ -1,13 +1,24 @@
 from flask import current_app
 import psycopg2
 import os
-from app import create_app
+
+
+# Local imports
+from fastfoodfast import queries
+from app.app import create_app
 
 def init_databse():
     # Initiate the db
     database_url = os.getenv("DATABASE_URL")
     conn = psycopg2.connect(database_url)
-    return conn
+    curr = conn.cursor()
+    try:
+        for query in queries:
+            curr.execute()
+        conn.commit()
+        return conn
+    except:
+        print("Fail")
 
 def dismantle():
     # Close db connections
@@ -17,4 +28,5 @@ def dismantle():
     conn.close()
 
 
+init_databse()
 
--- app/fastfoodfast.py
+++ app/fastfoodfast.py
@@ -1,6 +1,6 @@
 users_table = """
 CREATE TABLE IF NOT EXISTS users (
-    user_id serial, 
+    user_id serial PRIMARY KEY, 
     username VARCHAR(25) NOT NULL,
     email VARCHAR(25) NOT NULL,
     password VARCHAR(20) NOT NULL,
@@ -13,6 +13,29 @@ meals_table = """
 CREATE TABLE IF NOT EXISTS meals (
     name VARCHAR(25) NOT NULL,
     description VARCHAR(25) NOT NULL,
-    price 
+    price INT(6) NOT NULL,
+    meal_id serial,
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
 )
 """
+
+category_table = """
+CREATE TABLE IF NOT EXISTS categories (
+ category_id serial PRIMARY KEY,
+ category_name VARCHAR (25) NOT NULL
+)
+"""
+
+orders_table = """
+CREATE TABLE IF NOT EXISTS orders (
+    user_id INT NOT NULL,
+    ordered_date timestamp with time zone DEFAULT ('now'::text),
+    price INT(6) NOT NULL,
+    status VARCHAR (25) NOT NULL,
+    description VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+)
+"""
+
+queries = [users_table, meals_table, orders_table, category_table]
