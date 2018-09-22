b9685209b58873b5d361d733fce7158a77fdba49
wachira_Dev <31078302+tesh254@users.noreply.github.com>
2018-09-22 09:17:00 +0300

    Ch db connection 160654264 (#6)
    * Connected to db successfully
    [starts #160654264]
    
    * Minor changes and refactoring code
    [starts #160683365]
    [#160649365]
    
    * Requirements.txt update
    [#160683365]
    
    * Db conection done
    [#160654264]

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
-from flask import Flask 
+from flask import Flask, Blueprint
 from flask_restful import Api
-from instance import config
+
+# Local Import
+from instance.config import app_config
 from app.api.v1.views import Orders, OrdersManipulation, LandingPage
 
 def create_app(configuration):
 
     app = Flask(__name__)
-    app.config.from_object(config.app_config[configuration])
-
+    app.config.from_object(app_config[configuration])
+    #app.config.from_pyfile('config.py')
     api = Api(app)
-
     api.add_resource(LandingPage, '/')
     api.add_resource(Orders, '/api/v1/orders')
     api.add_resource(OrdersManipulation, '/api/v1/orders/<int:identifier>')
--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,3 +1,6 @@
+from flask import request
+import datetime as dt
+
 class OrdersOperation:
     
     def __init__(self):
@@ -7,31 +10,24 @@ class OrdersOperation:
         return self.orders
 
     def order(
-            self, order_id, username, 
-            products, product_name, 
-            product_price, product_qty, 
-            status, ordered_date, delivered_date):
+            self, order_id, username,
+            product_name, product_price, 
+            status, ordered_date, 
+            delivered_date):
             
         self.name = product_name,
         self.price = product_price
-        self.qty = product_qty
-        self.products = products
         self.id = order_id
         self.username = username
         self.ordered_date = ordered_date
         self.delivered_date = delivered_date
         self.status = status
 
-        self.products = {
-            "name":self.name,
-            "qty":self.qty,
-            "price":self.price
-        }
-
         self.order_fields = {
             "id":self.id,
             "username":self.username,
-            "products":self.products,
+            "product_name":self.name,
+            "product_price":self.price,
             "status":self.status,
             "ordered_date":self.ordered_date,
             "delivered_date":self.delivered_date
@@ -44,3 +40,21 @@ class OrdersOperation:
            return specific_order
         else:
             return None 
+
+    def save_order(self):
+        data_json = request.get_json()
+        new_order = {
+            "id":len(self.orders) + 1,
+            "username":data_json['username'],
+            "product_name":data_json["product_name"],
+            "product_price":data_json["product_price"],
+            "status":"Pending",
+            "ordered_date":str(dt.datetime.now()),
+            "delivered_date":None
+        }
+
+        self.orders.append(new_order)
+        return new_order
+
+
+
