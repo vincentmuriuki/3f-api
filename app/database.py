de0bba46f2c058d14e517bfd4790fc66f00644c7
tesh254 <ewachira254@gmail.com>
2018-09-24 19:55:36 +0300

    chore (description) Ignored the .vscode folder [#160683365]

--- app/database.py
+++ app/database.py
@@ -9,8 +9,8 @@ import psycopg2
 # Local imports
 from fastfoodfast import queries
 
-def init_databse():
-    # Initiate the db
+def init_database():
+    """ This method is used initialize database """
     url = os.environ.get('DATABASE_URL')
     conn = psycopg2.connect(url)
     #conn = psycopg2.connect("dbname='fastfoodtest' user='postgres' password='1234' host='localhost' port='5432'")
@@ -23,13 +23,39 @@ def init_databse():
     except Exception as e:
         print(e)
 
+def init_test_database():
+    """ This method is used to initialize test database """
+    url = os.environ.get("DATABASE_TEST_URL")
+    conn = psycopg2.connect(url)
+    cursor = conn.cursor()
+    dismantle()
+    try:
+        for query in queries:
+            cursor.execute(query)
+        conn.commit()
+        return conn
+    except Exception as e:
+        print(e)
+            
+
 def dismantle():
-    # Close db connections
-    database_url = os.getenv("DATABASE_URL")
-    conn = psycopg2.connect(database_url)
-    
-    conn.close()
+    """ Destroy all data in table in the test database """
+    url = os.getenv('DATABASE_TEST_URL')
+    conn = psycopg2.connect(url)
+    cursor = conn.cursor()
+    users = "DROO TABLE IF EXISTS users CASCADE"
+    meals = "DROP TABLE IF EXISTS meals CASCADE"
+    orders = "DROP TABLE IF EXISTS orders CASCADE"
+    category = "DROP TABLE IF EXISTS category CASCADE"
+
+    queries = [orders, meals, users, category]
+    try:
+        for query in queries:
+            cursor.execute(query)
+        conn.commit()
+    except Exception as e:
+        print(e)
+        
 
 
-init_databse()
 
