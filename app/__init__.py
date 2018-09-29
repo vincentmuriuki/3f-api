525db2f53552fcb4b787882bcf30a73fe771d2ad
tesh254 <ewachira254@gmail.com>
2018-09-29 12:15:45 +0300

    [Chore #160754506] Updated the user authentication endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 # Local Import
 from instance.config import app_config
 from app.api.v1.views import Orders, OrdersManipulation, LandingPage
-from app.api.v2.views.users import UserRegistration, UserLogin, UserLogout
+from app.api.v2.views.users import UserRegistration, UserLogin, UserLogout, User
 
 def create_app(configuration):
 
@@ -17,6 +17,8 @@ def create_app(configuration):
     api.add_resource(OrdersManipulation, '/api/v1/orders/<int:identifier>')
     api.add_resource(UserRegistration, '/api/v2/auth/signup')    
     api.add_resource(UserLogin, '/api/v2/auth/login')
+    api.add_resource(UserLogout, '/api/v2/auth/logout')
+    api.add_resource(User, '/api/v2/auth/profile')
 
 
     return app
