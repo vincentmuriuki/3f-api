96da264094e758c7fbf3f95bfb2055aa401d9607
tesh254 <ewachira254@gmail.com>
2018-10-01 17:19:33 +0300

    This branch contains the whole of challenge 2

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_restful import Api
 # Local Import
 from instance.config import app_config
 from app.api.v1.views import Orders, OrdersManipulation, LandingPage
-from app.api.v2.views.users import UserRegistration, UserLogin, UserLogout, User
 
 def create_app(configuration):
 
@@ -15,10 +14,5 @@ def create_app(configuration):
     api.add_resource(LandingPage, '/')
     api.add_resource(Orders, '/api/v1/orders')
     api.add_resource(OrdersManipulation, '/api/v1/orders/<int:identifier>')
-    api.add_resource(UserRegistration, '/api/v2/auth/signup')    
-    api.add_resource(UserLogin, '/api/v2/auth/login')
-    api.add_resource(UserLogout, '/api/v2/auth/logout')
-    api.add_resource(User, '/api/v2/auth/profile')
-
 
     return app
