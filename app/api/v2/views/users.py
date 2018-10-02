53b7e3888bb1c7d3d6a673c27d9190fafb752947
tesh254 <ewachira254@gmail.com>
2018-10-02 19:37:35 +0300

    [Feature #160927399] Solve heroku decoding problem

--- app/api/v2/views/users.py
+++ app/api/v2/views/users.py
@@ -76,7 +76,7 @@ class UserRegistration(Resource):
                 {
                     "status":"Success",
                     "message":"User created successfully",
-                    "auth_token":str(decode_token),
+                    "auth_token":str(decode_token).decode("utf-8"),
                     "user_id":user_id
                 }
             ), 201
