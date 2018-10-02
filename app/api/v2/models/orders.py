0c716ef635bb82e744ff91666f66eda68ad97fbd
wachira_Dev <31078302+tesh254@users.noreply.github.com>
2018-10-02 11:09:26 +0300

    [Feature #160755046] Getting specific order endpoint (#11)
    * [Chore #160754994] Done with order models
    
    * [Feature #160754994] The getting of all orders endpoints is done
    
    * [Feature #160754738] All endpoint for facilitating the creation of an order done
    
    * [Feature #160755106] Set up a method to update order status and delivered date
    
    * [Feature #160755106] Finished with the update order endpoint
    
    * [starts #160755046] Finished with getting a specific order
    
    * [Feature #160755046] Done with getting a specific order and updating the order status

--- app/api/v2/models/orders.py
+++ app/api/v2/models/orders.py
@@ -42,5 +42,12 @@ class OrderModels(object):
         status, description) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
         """ % (user_id, meal, ordered_date, price, qty, amount, status, description))
         self.db.commit()
-        
+    
+    def update_status(self, order_id, delivered_date):
+        curr = self.db.cursor()
+        curr.execute("""
+        UPDATE orders SET status='Delivered' AND delivered_date='%s'
+        WHERE order_id='%s'
+        """ % (delivered_date,order_id))
+        self.db.commit()
 
